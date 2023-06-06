dd3c329f26ff551d6ef0198fb26cf5e7e8d13caa
Alasdair Daniels <alasdair.daniels19@imperial.ac.uk>
2023-06-06 14:52:44 +0100

    Removed SDHMNode from __init__.py

--- hmt/__init__.py
+++ hmt/__init__.py
@@ -1,3 +1,3 @@
 from .core import Tree, Node, HMTError, HMTWarning, read_txt
-from .hidden_markov_tree import HMNode, SDHMNode, HMTree, HMForest
+from .hidden_markov_tree import HMNode, HMTree, HMForest
 # from .kalman import KalmanNode, KalmanTree, KalmanForest
