df5bca471252082d443ab8c5f6bf743b5fe4cad1
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2024-06-28 16:35:51 +0100

    Add example test

--- tests/test_hmt.py
+++ tests/test_hmt.py
@@ -1,8 +1,8 @@
 """Tests for the main module."""
 
-from hmt import __version__
-
 
 def test_version():
     """Check that the version is acceptable."""
+    from hmt import __version__
+
     assert __version__ == "0.1.0"
